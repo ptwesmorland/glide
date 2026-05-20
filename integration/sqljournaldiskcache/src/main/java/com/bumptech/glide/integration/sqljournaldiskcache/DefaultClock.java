e4c25e48d3eacc0bd7fbfadb5bafb4448b8f8719
Collins Mucho <72305974+sleepchild@users.noreply.github.com>
2026-05-20 07:17:36 +0200

    Delete integration directory

--- integration/sqljournaldiskcache/src/main/java/com/bumptech/glide/integration/sqljournaldiskcache/DefaultClock.java
+++
@@ -1,8 +0,0 @@
-package com.bumptech.glide.integration.sqljournaldiskcache;
-
-final class DefaultClock implements Clock {
-  @Override
-  public long currentTimeMillis() {
-    return System.currentTimeMillis();
-  }
-}
